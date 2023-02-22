2972f4f520baf2803892751f4c90f590aea00b96
CalvinRathin <113708133+CalvinRathin@users.noreply.github.com>
2023-02-22 18:55:59 +0530

    Add files via upload

---
+++ Tasks/Code First/TrainingMVC/Data/TrainingMVCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrainingMVC.Models;
+
+namespace TrainingMVC.Data
+{
+    public class TrainingMVCContext : DbContext
+    {
+        public TrainingMVCContext (DbContextOptions<TrainingMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrainingMVC.Models.Author> Author { get; set; } = default!;
+
+        public DbSet<TrainingMVC.Models.Book> Book { get; set; }
+    }
+}
---
+++ Tasks/Code First/TrainingMVC/Models/Author.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainingMVC.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorID { get; set; }
+        [Required]
+        public string? First_Name { get; set; }
+
+        public string? Last_Name { get; set; }
+        public string? Country { get; set; }
+
+       public  ICollection<Book> Books { get; set; }
+
+        //ICollection<BOOK> IS ADDED HERE TO AVOID REPETITION OF SAME AUTHOR ID
+        //  ,bECAUSE WE DONT NEED MULTPLE AUTHOR ID FOR SAME author.so we add here
+        //and not in books
+
+
+
+        // check create.cshtml , line 27 of code
+        //viewbag.AuthorID -> this line from create.cshtml
+        //      refers to the first name of the author
+    }
+}
