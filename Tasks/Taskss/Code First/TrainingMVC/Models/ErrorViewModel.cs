ffe1814578c9ec5f5a63224b459f57b5190e4add
CalvinRathin <113708133+CalvinRathin@users.noreply.github.com>
2023-02-22 18:49:31 +0530

    Delete ErrorViewModel.cs

--- Tasks/Taskss/Code First/TrainingMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrainingMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
